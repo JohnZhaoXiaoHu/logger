178e1c0ad0527f978fecc79db1bf44ca9062bcc5
IInfinity <DevinDon@Foxmail.com>
2019-04-09 16:17:31 +0800

    feat(demo): add simple demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,30 +1,8 @@
-import { Logger, logger } from '../main';
-import { Writable } from 'stream';
-import { createWriteStream } from 'fs';
+// import { logger } from '@iinfinity/logger';
+import { logger } from '../main'; // use above import statement in your workspace
 
-// logger mean this.
-// const logger = new Logger(process.stdout, process.stderr);
-
-logger.log('log');
+logger.log('log what you input without any decorate');
 logger.debug('debug', 'to', 'console');
 logger.error('something error');
 logger.info('info');
 logger.warn('warn', 'something');
-
-const myStream = new Writable({
-  write(chunk, encoding, callback) {
-    console.log(`This is my stream. ${chunk.toString()}`);
-    callback();
-  }
-});
-
-/** Stdout = myStream. */
-const myLogger1 = new Logger(myStream);
-/** Stderr = myStream. */
-const myLogger2 = new Logger(process.stdout, myStream);
-
-const fileStream = createWriteStream('dist/stream', { flags: 'a' });
-/** Write error information to file, and disable color mode. */
-const fileLogger = new Logger(process.stdout, fileStream, false);
-
-fileLogger.error('Write to file');
