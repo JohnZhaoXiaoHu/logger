4f9b3b62d18c7a990d9d76e528d5789b8bed312f
IInfinity <DevinDon@Foxmail.com>
2019-04-09 16:18:52 +0800

    test: add assert test & perf debug test

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,58 +1,90 @@
-import { Logger, logger } from '../main';
+import { Logger } from '../main';
 import { Writable } from 'stream';
+import { Level } from '../main/@types';
 
 describe('logger', () => {
 
-  let cusout: NodeJS.WritableStream;
-  let cuserr: NodeJS.WritableStream;
+  let cusout: Writable;
+  let cuserr: Writable;
   let out: string;
   let err: string;
-  let customLogger: Logger;
+  let customizeLogger: Logger;
 
   beforeAll(done => {
     cusout = new Writable({
       write(chunk, encoding, callback) {
         out = chunk.toString().trim();
-        console.log('CSUOUT: ' + out);
+        // console.log('CSUOUT: ' + out);
         callback();
       }
     });
     cuserr = new Writable({
       write(chunk, encoding, callback) {
         err = chunk.toString().trim();
-        console.error('CSUERR: ' + err);
+        // console.error('CSUERR: ' + err);
         callback();
       }
     });
-    customLogger = new Logger(cusout, cuserr);
+    customizeLogger = new Logger({
+      stdout: cusout,
+      stderr: cuserr,
+      colorful: false,
+      level: Level.ALL,
+      name: 'test'
+    });
     done();
   });
 
-  it('method: log.', done => {
-    const input1 = 'log';
-    const input2 = 'message';
-    expect((customLogger.log(input1), out)).toBe(input1);
-    expect((customLogger.log(input2), out)).toBe(input2);
-    expect((customLogger.log(input1, input2), out)).toBe(`${input1} ${input2}`);
-    const spy = spyOn(logger, 'log');
-    logger.log(input1, input2);
-    expect(spy).toHaveBeenCalledWith(input1, input2);
-    expect(spy).toHaveBeenCalledTimes(1);
+  beforeEach(done => {
+    customizeLogger.setLevel(Level.ALL);
+    out = '';
+    err = '';
     done();
   });
 
-  it('method: debug.', done => {
-    // const matcher = /\d+-\d+-\d+ \d+:\d+:\d+ \[DEBUG\] /;
-    const matcher = (message: any) => new RegExp(`\d+-\d+-\d+ \d+:\d+:\d+ \[DEBUG\] ${message}`);
+  it('default logger & customize logger should be created', done => {
+    expect(Logger.getLogger()).toBeDefined();
+    expect(Logger.getLogger() instanceof Logger).toBeTruthy();
+    expect(Logger.getLogger('customize')).toBeDefined();
+    expect(Logger.getLogger('customize') instanceof Logger).toBeTruthy();
+    done();
+  });
+
+  it('assert method should output correctly', done => {
+    const matcherTrue = /\d+-\d+-\d+ \d+:\d+:\d+ \[ASSERT\] same/;
+    const matcherFalse = /\d+-\d+-\d+ \d+:\d+:\d+ \[ASSERT\] different/;
+    // const matcher = (text: string) => /\d+-\d+-\d+ \d+:\d+:\d+ \[ASSERT\] ${}/;
+    expect((customizeLogger.assert(true, 'same', 'different'), matcherTrue.test(out))).toBeTruthy();
+    expect((customizeLogger.assert(false, 'same', 'different'), matcherFalse.test(err))).toBeTruthy();
+    done();
+  });
+
+  it('debug method should output correctly when level is not higher than `DEBUG`', done => {
+    const matcher = /\d+-\d+-\d+ \d+:\d+:\d+ \[DEBUG \] .*/;
+    // const matcher = (message: any) => new RegExp(`\d+-\d+-\d+ \d+:\d+:\d+ \[DEBUG \] ${message}`);
     const input1 = 'debug';
     const input2 = 'message';
-    expect((customLogger.debug(input1), matcher(input1).test(out))).toBeTruthy();
-    expect((customLogger.debug(input2), matcher(input2).test(out))).toBeTruthy();
-    expect((customLogger.debug(input1, input2), matcher(`${input1} ${input2}`).test(out))).toBeTruthy();
-    const spy = spyOn(logger, 'debug');
-    logger.debug(input1, input2);
+    customizeLogger.setLevel(Level.ALL);
+    expect((customizeLogger.debug(input1), matcher.test(out))).toBeTruthy();
+    customizeLogger.setLevel(Level.DEBUG);
+    expect((customizeLogger.debug(input2), matcher.test(out))).toBeTruthy();
+    expect((customizeLogger.debug(input1, input2), matcher.test(out))).toBeTruthy();
+    const spy = spyOn(customizeLogger, 'debug');
+    customizeLogger.debug(input1, input2);
     expect(spy).toHaveBeenCalledWith(input1, input2);
     expect(spy).toHaveBeenCalledTimes(1);
+    done();
+  });
+
+  it('debug method should not output when level is higher than `DEBUG`', done => {
+    customizeLogger.setLevel(Level.NONE);
+    expect((customizeLogger.debug('debug should not output anything'), out)).toBe('');
+    customizeLogger.setLevel(Level.ERROR);
+    expect((customizeLogger.debug('debug should not output anything'), out)).toBe('');
+    customizeLogger.setLevel(Level.WARN);
+    expect((customizeLogger.debug('debug should not output anything'), out)).toBe('');
+    customizeLogger.setLevel(Level.INFO);
+    expect((customizeLogger.debug('debug should not output anything'), out)).toBe('');
     done();
   });
 
