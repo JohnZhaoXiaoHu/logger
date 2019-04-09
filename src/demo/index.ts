db5dc6c230b6de00a6e6553711d86a426b8cf373
IInfinity <DevinDon@Foxmail.com>
2019-04-09 16:22:10 +0800

    feat(demo): add assert demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,6 +1,8 @@
 // import { logger } from '@iinfinity/logger';
 import { logger } from '../main'; // use above import statement in your workspace
 
+logger.assert(true, 'logger assert true', 'logger assert false');
+logger.assert(false, 'logger assert true', 'logger assert false');
 logger.log('log what you input without any decorate');
 logger.debug('debug', 'to', 'console');
 logger.error('something error');
