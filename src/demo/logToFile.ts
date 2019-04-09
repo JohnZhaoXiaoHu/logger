3cfcc66db29fe218f9af1b5a26c594bfc39c3884
IInfinity <DevinDon@Foxmail.com>
2019-04-10 00:26:31 +0800

    feat(demo): add log to file only demo

--- src/demo/logToFile.ts
+++ src/demo/logToFile.ts
@@ -1,15 +1,32 @@
 // import { Logger } from '@iinfinity/logger';
 import { Logger } from '../main'; // use above import statement in your workspace
 
-// save log to file
-const logToFile = new Logger({
+/** Save logs to file(s) only, it will not display on screen. */
+const logToFileOnly = new Logger({
+  name: 'log to file only',
   fileout: 'out.log',
   fileerr: 'err.log'
 });
 
-logToFile.log('log what you input without any decorate');
-logToFile.debug('debug', 'to', 'console');
-logToFile.error('something error');
-logToFile.info('info');
-logToFile.warn('warn', 'something');
-logToFile.info('And now, open your out.log & err.log file to see these words.');
+logToFileOnly.log('log what you input without any decorate');
+logToFileOnly.debug('debug', 'to', 'file only');
+logToFileOnly.error('something error file only');
+logToFileOnly.info('info file only');
+logToFileOnly.warn('warn', 'something', 'file only');
+logToFileOnly.info('And now, open your out.log & err.log file to see these words file only.');
+
+/** Save logs to file(s) & screen. */
+const logToFileAndScreen = new Logger({
+  name: 'log to file and screen',
+  stdout: process.stdout,
+  stderr: process.stderr,
+  fileout: 'out.log',
+  fileerr: 'err.log'
+});
+
+logToFileAndScreen.log('log what you input without any decorate');
+logToFileAndScreen.debug('debug', 'to', 'console');
+logToFileAndScreen.error('something error');
+logToFileAndScreen.info('info');
+logToFileAndScreen.warn('warn', 'something');
+logToFileAndScreen.info('And now, open your out.log & err.log file to see these words.');
