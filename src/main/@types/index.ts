6963f47132c082ddff329612715b16cf68723afe
IInfinity <DevinDon@Foxmail.com>
2019-04-09 16:16:27 +0800

    feat(@types): add type declare

--- src/main/@types/index.ts
+++ src/main/@types/index.ts
@@ -1,11 +1,52 @@
-export interface Level {
-  [index: string]: Style;
-}
+export * from './const';
+import { Writable } from 'stream';
 
+/** Color. */
 export type Color = string;
 
+/** Log level. */
+export enum Level {
+  ALL,
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+  NONE
+}
+
+/** Console option. */
+export interface Option {
+  /** Colorful console, default to true. */
+  colorful: boolean;
+  /** Path to save error. */
+  fileerr?: string;
+  /** Path to save output. */
+  fileout?: string;
+  /** Log level, default to debug. */
+  level: Level;
+  /** Logger name, default to 'default'. */
+  name: string;
+  /** Standard output. */
+  stdout: Writable;
+  /** Standard error. */
+  stderr: Writable;
+}
+
+/** Style. */
 export interface Style {
   background: Color;
-  color: Color;
+  font: Color;
   name: string;
+}
+
+/** Styles. */
+export interface Styles {
+  ASSERT: {
+    TRUE: Style;
+    FALSE: Style;
+  };
+  DEBUG: Style;
+  INFO: Style;
+  WARN: Style;
+  ERROR: Style;
 }
